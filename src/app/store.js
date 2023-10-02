025f37a0547a4cbc386668ee042b17659e30f9a1
Shamim <shamimhossen3912@gmail.com>
2023-10-02 11:25:44 -0700

    feat: tag selected feature

--- src/app/store.js
+++ src/app/store.js
@@ -3,6 +3,7 @@ import videosReducer from "../features/videos/videosSlice";
 import tagsReducer from "../features/tags/tagsSlice";
 import videoReducer from "../features/video/videoSlice";
 import relatedVideosReducer from "../features/relatedVideos/relatedVideosSlcie";
+import filterReducer from "../features/filter/filterSlice";
 
 export const store = configureStore({
   reducer: {
@@ -10,5 +11,6 @@ export const store = configureStore({
     tags: tagsReducer,
     video: videoReducer,
     relatedVideos: relatedVideosReducer,
+    filter: filterReducer,
   },
 });
--- src/components/video_tags/Tag.jsx
+++ src/components/video_tags/Tag.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 
+import { useDispatch, useSelector } from "react-redux";
+import { tagRemoved, tagSelected } from "../../features/filter/filterSlice";
+
 const Tag = ({ tag = {} }) => {
+  const dispatch = useDispatch();
+  const { tags: selectedTags } = useSelector((state) => state.filter);
   const { title } = tag;
+
+  let isSelected = selectedTags.includes(title) ? true : false;
+
+  const handleSelected = () => {
+    if (isSelected) {
+      dispatch(tagRemoved(title));
+    } else {
+      dispatch(tagSelected(title));
+    }
+  };
+
+  const style = isSelected
+    ? "bg-blue-600 text-white px-4 py-1 rounded-full cursor-pointer"
+    : "px-4 py-1 text-blue-600 bg-blue-100 rounded-full cursor-pointer";
   return (
     <>
-      <div className="px-4 py-1 text-blue-600 bg-blue-100 rounded-full cursor-pointer">
+      <div className={style} onClick={handleSelected}>
         {title}
       </div>
     </>
